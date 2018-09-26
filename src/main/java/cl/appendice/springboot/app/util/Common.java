3575f799790e20ee1047ed11ced93a681397f9c2
Breaker <antivilo.trabajo@gmail.com>
2018-09-26 00:07:52 -0300

    commit inicial

---
+++ src/main/java/cl/appendice/springboot/app/util/Common.java
@@ -0,0 +1,12 @@
+package cl.appendice.springboot.app.util;
+
+public class Common {
+	public static boolean isNumeric(String str) {
+		try {
+			double d = Double.parseDouble(str);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+}
